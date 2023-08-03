5ffb668c75830d40a54207c67739466d7c6cc12b
Peter Stratton <peter@alternative.co>
2023-08-03 17:37:20 -0400

    first commit

---
+++ src/Team.svelte
@@ -0,0 +1,30 @@
+<script>
+    export let team;
+    export let score;
+    export let gameOver;
+
+    function increment() {
+        score += 1;
+    }
+
+    function decrement() {
+        score -= 1;
+    }
+</script>
+
+<div style="border: dashed {team.toLowerCase()} 5px" class="col">
+    <h2 
+        class:text-danger={team === 'Red'}
+        class:text-primary={team === 'Blue'}>
+        Team {team}: {score}
+    </h2>
+    <button disabled={gameOver} on:click={increment} class="btn btn-success">+</button>
+    <button disabled={gameOver} on:click={decrement} class="btn btn-danger">-</button>
+</div>
+
+<style>
+    div.col {
+        padding: 20px;
+        text-align: center;
+    }
+</style>
